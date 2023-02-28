ab2c594863e9e11bd79e0a721c87195ba105b6de
Yajat0811 <97752332+Yajat0811@users.noreply.github.com>
2023-02-28 13:19:40 +0530

    Update sample.py
    this is updated main code file for which a self_add function created for menu option add and whole menu created

--- sample.py
+++ sample.py
@@ -1,14 +1,25 @@
 # import tkinter as tk
 from tkinter import *
-from functools import partial
+#from functools import partial
 def validateLogin(username, password):
 	print("username entered :", username.get())
 	print("password entered :", password.get())
 	return
 def my_window():
     top=Toplevel()
-    c=Button(top,text="Update")
-    c.pack()
+    #top.deiconify()
+    frame=Frame(top)
+    frame.pack()
+    add=Button(frame,text="ADD",command=self_add)
+    add.grid(row=0,column=0)
+    update=Button(frame,text="UPDATE")
+    update.grid(row=1,column=0)
+    view_all=Button(frame,text="VIEW ALL")
+    view_all.grid(row=2,column=0)
+    view_self=Button(frame,text="VIEW SELF")
+    view_self.grid(row=3,column=0)
+    
+    
 
 root=Tk()
 root.geometry('400x150')  
@@ -22,7 +33,8 @@ password=StringVar()
 e=Label(frame,text="Password:")
 e.grid(row=1,column=0)
 e1=Entry(frame,textvariable=username).grid(row=0,column=1)
-e2=Entry(frame,textvariable=password).grid(row=1,column=1)
-b=Button(root,text="Login",command=my_window())
+e2=Entry(frame,textvariable=password,show="*").grid(row=1,column=1)
+b=Button(root,text="Login",command=my_window)
+
 b.pack()
 root.mainloop()
